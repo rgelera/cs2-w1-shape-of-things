6ebd30cce5d813624047a43174fb7368aa2e52bb
Rodney Gelera <rodney.gelera@shopify.com>
2020-09-07 21:40:07 -0700

    first 5 rows

--- shape-of-things.cpp
+++ shape-of-things.cpp
@@ -2,11 +2,24 @@
 
 using std::cout;
 
-int main(int argc, char** argv) {
-  for (int row = 1; row <= 10; row++) {
-    for (int hashNum = 1; hashNum <= 9; hashNum++) {
+void printNumOfHash(int i) {
+  for (int hashNum = 1; hashNum <= i; hashNum++) {
       cout << "#";
+  }
+}
+
+int main(int argc, char** argv) {
+  int spaceNum;
+
+  for (int row = 1; row <= 5; row++) {
+    for (int spaceBNum = 0; spaceBNum <= row - 1; spaceBNum++) {
+      cout << " ";
     }
+    printNumOfHash(row);
+    for (int spaceMNum = 1; spaceMNum <= 20 - 4 * row; spaceMNum++) {
+      cout << " ";
+    }
+    printNumOfHash(row);
     cout << "\n";
   }
 }
