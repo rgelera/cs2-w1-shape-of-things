91ed59cb9004055e9780aa2b0037a8a8e6b5c1d1
Rodney Gelera <rodney.gelera@shopify.com>
2020-09-07 21:11:06 -0700

    9 by 10 block

--- shape-of-things.cpp
+++ shape-of-things.cpp
@@ -3,5 +3,10 @@
 using std::cout;
 
 int main(int argc, char** argv) {
-  
+  for (int row = 1; row <= 10; row++) {
+    for (int hashNum = 1; hashNum <= 9; hashNum++) {
+      cout << "#";
+    }
+    cout << "\n";
+  }
 }
